ae2486a48edcc740bac3c382e5b5febd660965c3
Satyam Gaur <satyam.gaur@sprinklr.com>
2022-06-13 13:00:11 +0530

    adds click functionality on minimap

--- src/Minimap.js
+++ src/Minimap.js
@@ -1,21 +1,43 @@
 import React from 'react';
+import { useRef, useEffect } from 'react';
 
 const MINIMAP_WIDTH = 350;
 const MINIMAP_HEIGHT = 250;
-const MINIMAP_PADDING = 20;
 
-const Minimap = ({ vertices, extremeX, extremeY, viewport }) => {
+const Minimap = ({ vertices, extremeX, extremeY, viewport, changeScrollHandler }) => {
   const scalingFactor = {
     x: MINIMAP_WIDTH / extremeX,
     y: MINIMAP_HEIGHT / extremeY,
   };
 
+  viewport.width = viewport.xMax - viewport.xMin;
+  viewport.height = viewport.yMax - viewport.yMin;
+
   const minimapViewport = {
     top: viewport.yMin * scalingFactor.y,
     left: viewport.xMin * scalingFactor.x,
-    width: Math.min((viewport.xMax - viewport.xMin) * scalingFactor.x, MINIMAP_WIDTH + 2 * MINIMAP_PADDING),
-    height: Math.min((viewport.yMax - viewport.yMin) * scalingFactor.y, MINIMAP_HEIGHT + 2 * MINIMAP_PADDING),
+    width: Math.min(viewport.width * scalingFactor.x, MINIMAP_WIDTH),
+    height: Math.min(viewport.height * scalingFactor.y, MINIMAP_HEIGHT),
   };
+
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const element = ref.current;
+    const handleClick = event => {
+      const newScrollPosition = {
+        scrollLeft: (event.clientX - element.offsetLeft - (viewport.width * scalingFactor.x) / 2) / scalingFactor.x,
+        scrollTop: (event.clientY - element.offsetTop - (viewport.height * scalingFactor.y) / 2) / scalingFactor.y,
+      };
+      changeScrollHandler(newScrollPosition);
+    };
+
+    element.addEventListener('click', handleClick);
+
+    return () => {
+      element.removeEventListener('click', handleClick);
+    };
+  });
 
   const nodes = vertices.map(vertex => {
     return (
@@ -37,6 +59,7 @@ const Minimap = ({ vertices, extremeX, extremeY, viewport }) => {
   return (
     <div
       className="minimap"
+      ref={ref}
       style={{
         boxSizing: 'content-box',
         border: '2px solid black',
@@ -46,7 +69,6 @@ const Minimap = ({ vertices, extremeX, extremeY, viewport }) => {
         position: 'fixed',
         bottom: '20px',
         right: '20px',
-        padding: `${MINIMAP_PADDING}px`,
       }}
     >
       {nodes}
